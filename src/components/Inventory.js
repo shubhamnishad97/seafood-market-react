b0f6d6f2433ad6f322a8ec8b65e33c56766b1824
Shubham Nishad <shubham.nishad@mitel.com>
2020-12-25 18:16:14 -0500

    add sample data

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,7 +7,9 @@ class Inventory extends React.Component {
             <div className="inventory">
                 <h2>Inventory</h2>
                 <AddFishForm addFish={this.props.addFish} />
-                <button onClick={}>Load Sample Fishes</button>
+                <button onClick={this.props.loadSampleFishes}>
+                    Load Sample Fishes
+                </button>
             </div>
         );
     }
