8c92917ec3ac8ea72c6d2628eb418260163a2489
Shubham Nishad <shubham.nishad@mitel.com>
2021-01-05 21:44:52 -0500

    did formatting again, remove stuff

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const NotFound = () => (
-  <div>
-    <h2>Not Found!?!!!!1111</h2>
-  </div>
+    <div>
+        <h2>Page not found</h2>
+    </div>
 );
 
 export default NotFound;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,15 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import StorePicker from "./StorePicker";
-import App from "./App";
-import NotFound from "./NotFound";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from './App';
+import NotFound from './NotFound';
 
 const Router = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={StorePicker} />
-      <Route path="/store/:storeId" component={App} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={App} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
 );
 
 export default Router;
