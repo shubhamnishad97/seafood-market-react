429df8a5ef2089054d80596c27f1d6c816935f4a
Shubham Nishad <shubham.nishad@mitel.com>
2021-01-05 23:16:41 -0500

    Revert "update img url"
    This reverts commit d6651f241316f913c564a12dea693c605840080b.

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -2,8 +2,7 @@
 const fishes = {
     fish1: {
         name: 'Pacific Halibut',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/hali.jpg',
+        image: '/images/hali.jpg',
         desc:
             'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
         price: 1724,
@@ -12,8 +11,7 @@ const fishes = {
 
     fish2: {
         name: 'Lobster',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/lobster.jpg',
+        image: '/images/lobster.jpg',
         desc:
             'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
         price: 3200,
@@ -22,8 +20,7 @@ const fishes = {
 
     fish3: {
         name: 'Sea Scallops',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/scallops.jpg',
+        image: '/images/scallops.jpg',
         desc:
             'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
         price: 1684,
@@ -32,8 +29,7 @@ const fishes = {
 
     fish4: {
         name: 'Mahi Mahi',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/mahi.jpg',
+        image: '/images/mahi.jpg',
         desc:
             'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
         price: 1129,
@@ -42,8 +38,7 @@ const fishes = {
 
     fish5: {
         name: 'King Crab',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/crab.jpg',
+        image: '/images/crab.jpg',
         desc:
             'Crack these open and enjoy them plain or with one of our cocktail sauces',
         price: 4234,
@@ -52,8 +47,7 @@ const fishes = {
 
     fish6: {
         name: 'Atlantic Salmon',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/salmon.jpg',
+        image: '/images/salmon.jpg',
         desc:
             'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
         price: 1453,
@@ -62,8 +56,7 @@ const fishes = {
 
     fish7: {
         name: 'Oysters',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/oysters.jpg',
+        image: '/images/oysters.jpg',
         desc:
             'A soft plump oyster with a sweet salty flavor and a clean finish.',
         price: 2543,
@@ -72,8 +65,7 @@ const fishes = {
 
     fish8: {
         name: 'Mussels',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/mussels.jpg',
+        image: '/images/mussels.jpg',
         desc:
             'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
         price: 425,
@@ -82,8 +74,7 @@ const fishes = {
 
     fish9: {
         name: 'Jumbo Prawns',
-        image:
-            'https://github.com/shubhamnishad97/food-ordering-react/blob/main/public/images/prawns.jpg',
+        image: '/images/prawns.jpg',
         desc:
             'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
         price: 2250,
